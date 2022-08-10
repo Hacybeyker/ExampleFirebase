c449c24b767f15e66591a211da209aaab26aa8c9
Carlos Osorio <hacybeyker@gmail.com>
2022-08-10 14:35:32 -0500

    Initial commit

---
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -0,0 +1,13 @@
+object ConfigureApp {
+    //TODO change values with your info [APP]
+    const val urlRepoDependencies = "https://maven.pkg.github.com/your_user/your_path_module"
+
+    //TODO change values with your info [SonarQube]
+    const val organization = "your_organization"
+    const val projectName = "app-scaffolding_android_main-android"
+
+    //TODO change values with your info [APP]
+    const val applicationId = "com.your.package"
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+}
